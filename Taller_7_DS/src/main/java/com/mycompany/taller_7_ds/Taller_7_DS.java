751116e32bd419f8de5b70ae89b94b2f3ac0cbc6
DanielMateoOrellana <107244270+DanielMateoOrellana@users.noreply.github.com>
2023-12-06 21:21:35 -0500

    Initial Commit

---
+++ Taller_7_DS/src/main/java/com/mycompany/taller_7_ds/Taller_7_DS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller_7_ds;
+
+/**
+ *
+ * @author Daniel i7 pro
+ */
+public class Taller_7_DS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
